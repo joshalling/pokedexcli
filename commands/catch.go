646f29c0e83065304b8dee3cb7f836de00d7ff53
Josh Alling <joshralling@gmail.com>
2025-05-26 08:28:31 -0700

    catch command

---
+++ commands/catch.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func Catch(c *Config, args []string) error {
+	id := args[0]
+
+	pokemon, err := c.Client.GetPokemon(id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+	baseChance := 800
+	result := rand.Intn(baseChance)
+	if result > pokemon.BaseExperience {
+		fmt.Printf("%s was caught!\n", pokemon.Name)
+	} else {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+	}
+
+	return nil
+}
